40e1bef80d8b2c46ee44683fd5c7d318f015670b
Noah Price <noah.price.237@gmail.com>
2025-06-24 00:35:03 -0400

    Create dev-stop.sh
    Add dev-stop for steamdeck.

---
+++ dev-stop.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Start up services for steamdeck dev
+set -e # exit on error
+
+
+# Check if user is running as root or with sudo
+# We need sudo to use systemctl gracefully
+if [[$EUID -ne 0]]; then
+    echo "This script requires root priveleges. Please run with sudo:"
+    echo "sudo $0"
+    exit 1
+fi
+
+echo "Stopping Tailscale..."
+
+tailscale down
+systemctl stop tailscaled
+
+if ! systemctl is-active --quiet tailscaled; then
+    echo "Tailscale is now stopped.."
+else
+    echo "Failed to stop tailscale."
+    exit 1
+fi
+
+echo "Tailscale stopped successfully."
+
+
+
+
